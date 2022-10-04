b786d39227c047cf7d61443702ab36bc22d1056d
kangbohyeon <qhgus535@naver.com>
2022-10-04 19:34:32 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import ImportConponent from './example/R003_ImportConponent';
+import LifecycleEx from './example/R004_LifecycleEx'
+import LifecycleEx2 from './example/R005_LifecycleEx';
+import LifecycleEx3 from './example/R006_LifecycleEx';
+import LifecycleEx4 from './example/R007_LifecycleEx';
+import LifecycleEx5 from './example/R008_LifecycleEx';
+import Es6 from './example/R009_LifecycleEx';
+import Variable from './example/R010_Variable';
+function App() {
+  return (
+    <div>
+      <h1>Start React 200!</h1>
+      <p>CSS 적용하기</p>
+      {/* <ImportConponent></ImportConponent>
+      <LifecycleEx></LifecycleEx>
+      <LifecycleEx2></LifecycleEx2>
+      <LifecycleEx5 prop_value = 'FromApp.js'/> */}
+      {/* <Es6/> */}
+      <Variable/>
+    </div>
+  );
+}
+
+export default App;
