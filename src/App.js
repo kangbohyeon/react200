22f3c170ae88f9148352af899d5ad5aaebdb3793
kangbohyeon <qhgus535@naver.com>
2022-10-05 17:33:54 +0900

    git test

--- src/App.js
+++ src/App.js
@@ -8,17 +8,19 @@ import LifecycleEx4 from './example/R007_LifecycleEx';
 import LifecycleEx5 from './example/R008_LifecycleEx';
 import Es6 from './example/R009_LifecycleEx';
 import Variable from './example/R010_Variable';
+import SpreadOperator from './example/R011_SpreadOperator';
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      {/* <ImportConponent></ImportConponent>
-      <LifecycleEx></LifecycleEx>
-      <LifecycleEx2></LifecycleEx2>
-      <LifecycleEx5 prop_value = 'FromApp.js'/> */}
+      {/* <ImportConponent></ImportConponent> */}
+      {/* <LifecycleEx></LifecycleEx> */}
+      {/* <LifecycleEx2></LifecycleEx2> */}
+      {/* <LifecycleEx5 prop_value = 'FromApp.js'/> */}
       {/* <Es6/> */}
-      <Variable/>
+      {/* <Variable/> */}
+      <SpreadOperator/>
     </div>
   );
 }
