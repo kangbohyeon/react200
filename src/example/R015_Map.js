27416234f258ebcb524ed763f8c6352e707a5683
kangbohyeon <qhgus535@naver.com>
2022-10-06 01:25:43 +0900

    ..

--- src/example/R015_Map.js
+++ src/example/R015_Map.js
@@ -1,27 +1,27 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 
-class R015_Map extends Component{
+class R015_Map extends Component {
 
-    componentDidMount(){
-        var Map_Arr = [3,2,8,8];
-        let Map_newArr = Map_Arr.map(x=>x)
-        console.log("1. Mapp_newArr : ["+Map_newArr+"]")
+    componentDidMount() {
+        var Map_Arr = [3, 2, 8, 8];
+        let Map_newArr = Map_Arr.map(x => x)
+        console.log("1. Mapp_newArr : [" + Map_newArr + "]")
 
-        let Map_mulitiArr = Map_Arr.map(x=>x*2)
-        console.log("2. Map_mulitiArr : ["+Map_mulitiArr+"]")
+        let Map_mulitiArr = Map_Arr.map(x => x * 2)
+        console.log("2. Map_mulitiArr : [" + Map_mulitiArr + "]")
 
-        var ObjArray = [{key:'react', value:'200'},
-                        {key:'리액트',value : 'TwoHundred'}];
-        let Map_objArr = ObjArray.map((obj,index) => {
-            console.log((index+3)+". obj : "+JSON.stringify(obj))
-            var Obj={};
+        var ObjArray = [{ key: 'react', value: '200' },
+        { key: '리액트', value: 'TwoHundred' }];
+        let Map_objArr = ObjArray.map((obj, index) => {
+            console.log((index + 3) + ". obj : " + JSON.stringify(obj))
+            var Obj = {};
             Obj[obj.key] = obj.value;
             return Obj;
         });
-        console.log("5. Map_objArr : " +JSON.stringify(Map_objArr))
+        console.log("5. Map_objArr : " + JSON.stringify(Map_objArr))
     }
 
-    render(){
+    render() {
         return (
             <h2>[THIS IS Map]</h2>
         )
