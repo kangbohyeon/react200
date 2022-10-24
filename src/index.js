ae0cfaf73c65b9b5b2f8055ddf54627c1ca4ba30
kangbohyeon <qhgus535@naver.com>
2022-10-24 19:24:36 +0900

    2022.10.24 study

--- src/index.js
+++ src/index.js
@@ -15,20 +15,88 @@
 // reportWebVitals();
 
 
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import './index.css'
+// import App from "./App";
+// import { createStore } from 'redux';
+// import reducers from './reducers';
+// const store = createStore(reducers);
+
+// const listener = () => {
+//     ReactDOM.render(
+//         <App store={store} />,
+//         document.getElementById('root')
+//     );
+// };
+
+// store.subscribe(listener);
+// listener();
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import './index.css';
+// import App from './App';
+// import { createStore} from 'redux';
+// import { Provider } from 'react-redux'
+// import reducers from './reducers';
+
+// const store = createStore(reducers);
+// const listener = () => {
+//     ReactDOM.render(
+//         <Provider store={store}>
+//             <App indexProp="react" />
+//         </Provider>,
+//         document.getElementById('root')
+//     );
+// };
+
+// store.subscribe(listener);
+// listener();
+
+// import React from "react";
+// import  ReactDOM  from "react-dom";
+// import './index.css';
+// import App from './components/App';
+// import { legacy_createStore as createStore, applyMiddleware } from "redux";
+// import { Provider } from "react-redux";
+// import reducers from './reducers';
+
+// const CallMiddleware = () => store => nextMiddle => action => {
+//     console.log('1. reducer 실행 전');
+//     console.log('2. action.type : '+action.type+', store str : '+store.getState().data.str);
+//     let result = nextMiddle(action);
+//     console.log('3. reducer 실행 후');
+//     console.log('4. action.type : ' + action.type+', store str : ' + store.getState().data.str);
+//     return result;
+// }
+
+
+// const store = createStore(reducers,  applyMiddleware(CallMiddleware));
+// const listener = () => {
+//     ReactDOM.render(
+//         <Provider store={store}>
+//             <App indexProp="react"/>
+//         </Provider>,
+//         document.getElementById('root')
+//     );
+// };
+
+// store.subscribe(listener);
+// listener();
+
+
 import React from "react";
 import ReactDOM from "react-dom";
-import './index.css'
-import App from "./App";
-import { createStore } from 'redux';
-import reducers from './reducers';
-const store = createStore(reducers);
+import { BrowserRouter } from "react-router-dom";
+import './index.css';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
 
-const listener = () => {
-    ReactDOM.render(
-        <App store={store} />,
-        document.getElementById('root')
-    );
-};
+ReactDOM.render((
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+), document.getElementById('root'));
 
-store.subscribe(listener);
-listener();
+serviceWorker.unregister();
