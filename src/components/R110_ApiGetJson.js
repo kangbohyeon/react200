4672059aa301a2be2f00cf6f68adcce8f28134bb
kangbohyeon <qhgus535@naver.com>
2022-11-01 23:06:29 +0900

    2022.11.01 study

--- src/components/R110_ApiGetJson.js
+++ src/components/R110_ApiGetJson.js
@@ -1,16 +1,15 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class R110_ApiGetJson extends Component {
     componentDidMount = async () => {
         const response = await fetch('/users');
-        const body = await response;
-        console.log(body);
+        const body = await response.json();
         console.log("body.message : " + body.message)
     }
 
     render() {
         return (
-            <><h1>Call Node Api get</h1></>
+            <><h1>Call Node Api Get</h1></>
         )
     }
 }
