aa7fdfd3a7d42b8f90ce910fb5f7402e1ec9160f
kangbohyeon <qhgus535@naver.com>
2022-11-02 17:33:14 +0900

    2022.11.02 study

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send({'message':'node get success'});
-});
-
-module.exports = router;
